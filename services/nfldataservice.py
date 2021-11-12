17eb6b8cfd3ae0e9f0dfa9ea9108f33b6139ab28
Aaron Johnson <aaronjohnson@utexas.edu>
2021-11-11 20:44:06 -0600

    add odds and setup some animation stuff

--- services/nfldataservice.py
+++ services/nfldataservice.py
@@ -8,16 +8,22 @@ from pytz import timezone
 
 TIMEZONE = timezone('US/Eastern')
 
+
 def convertWatchToEmbedUrl(watch):
     beg_index = watch.index('nfl-')
     end_index = watch.index('.html')
     id = int(watch[beg_index+4:end_index])
-    
+
     embed_id = '' if id == 1 else f'-{id}'
     embed_url = f'http://bfst.to/embe/nfl{embed_id}.php'
     return id, embed_url
 
+
 class NFLGame:
+    pass
+
+
+class TeamInfo:
     pass
 
 
@@ -44,15 +50,19 @@ class NFLParser(HTMLParser):
                     if attr[0] == 'href' and 'buff-streamz.com/watch' in attr[1]:
                         self.parsing_game = True
                         self.curr_game = NFLGame()
+                        self.curr_game.home_team = TeamInfo()
+                        self.curr_game.away_team = TeamInfo()
                         self.date_info = DateInfo()
-                        self.curr_game.id, self.curr_game.embed_link = convertWatchToEmbedUrl(attr[1])
+                        self.curr_game.id, self.curr_game.embed_link = convertWatchToEmbedUrl(
+                            attr[1])
 
     def handle_endtag(self, tag):
         if tag == 'a':
             if self.nested_level == 0:
                 if self.parsing_game:
                     self.parsing_game = False
-                    full_datetime = self.date_info.date.combine(self.date_info.date, self.date_info.time.time())
+                    full_datetime = self.date_info.date.combine(
+                        self.date_info.date, self.date_info.time.time())
                     self.curr_game.date = TIMEZONE.localize(full_datetime)
                     self.all_games.append(self.curr_game)
             else:
@@ -62,30 +72,55 @@ class NFLParser(HTMLParser):
         data = data.strip()
         if ' vs ' in data:
             teams = data.split(' vs ')
-            self.curr_game.away_team = teams[0]
-            self.curr_game.home_team = teams[1]
+            self.curr_game.away_team.name = teams[0]
+            self.curr_game.home_team.name = teams[1]
         elif str(self.curr_year) in data:
             self.date_info.date = datetime.strptime(data, '%Y-%m-%d')
         elif 'EST' in data:
-            formatted = data.upper().replace('EST','').strip()
+            formatted = data.upper().replace('EST', '').strip()
             self.date_info.time = datetime.strptime(formatted, '%I:%M %p')
+
 
 def _gameHasNotAlreadyEnded(game):
     game_end = game.date + timedelta(hours=4)
     now = datetime.now(TIMEZONE)
     return now < game_end
 
+
+def _parseBovadaData(game_list):
+    bovada_data = requests.get(
+        'https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl').json()[0]['events']
+    for event in bovada_data:
+        for game in game_list:
+            if game.home_team.name in event['description'] and game.away_team.name in event['description']:
+                outcomes = event['displayGroups'][0]['markets'][0]['outcomes']
+                for outcome in outcomes:
+                    odds = float(outcome['price']['decimal'])
+                    if outcome['description'] == game.home_team.name:
+                        game.home_team.odds = odds
+                    elif outcome['description'] == game.away_team.name:
+                        game.away_team.odds = odds
+
+
 def getJsonData():
     r = requests.get("http://buff-streamz.com/nfl-streams")
     parser = NFLParser()
     parser.feed(r.text)
 
+    _parseBovadaData(parser.all_games)
+
     add_test_games = False
     if add_test_games:
         Object = lambda **kwargs: type("Object", (), kwargs)
-        parser.all_games.append(Object(home_team='Test 1', away_team='Test Away 1', id= 154, embed_link= 'http://bfst.to/embe/nfl.php', date='1234'))
-        parser.all_games.append(Object(home_team='Test 2', away_team='Test Away 2', id= 155, embed_link= 'http://bfst.to/embe/nfl.php', date='1234'))
-        parser.all_games.append(Object(home_team='Test 3', away_team='Test Away 3', id= 156, embed_link= 'http://bfst.to/embe/nfl.php', date='1234'))
-    
+        parser.all_games.append(Object(home_team=Object(name='Test Home 1', odds=1.23456), away_team=Object(name='Test Away 1', odds=1.23456),
+                                id=154, embed_link='http://bfst.to/embe/nfl.php', date=datetime.now(TIMEZONE)))
+        parser.all_games.append(Object(home_team=Object(name='Test Home 2', odds=1.23456), away_team=Object(name='Test Away 2', odds=1.23456),
+                                id=155, embed_link='http://bfst.to/embe/nfl.php', date=datetime.now(TIMEZONE)))
+        parser.all_games.append(Object(home_team=Object(name='Test Home 3', odds=1.23456), away_team=Object(name='Test Away 3', odds=1.23456),
+                                id=156, embed_link='http://bfst.to/embe/nfl.php', date=datetime.now(TIMEZONE)))
+
     filtered_games = filter(_gameHasNotAlreadyEnded, parser.all_games)
-    return dict({'data': list(map(lambda game: dict({'id': game.id, 'home_team': game.home_team, 'away_team': game.away_team, 'embed_link': game.embed_link, 'date': game.date}), filtered_games))})
+    return dict({'data': list(map(lambda game: dict({'id': game.id, 'home_team': {'name': game.home_team.name, 'odds': game.home_team.odds if hasattr(game.home_team, 'odds') else None}, 'away_team': {'name': game.away_team.name, 'odds': game.away_team.odds if hasattr(game.away_team, 'odds') else None}, 'embed_link': game.embed_link, 'date': game.date}), filtered_games))})
+
+
+getJsonData()
